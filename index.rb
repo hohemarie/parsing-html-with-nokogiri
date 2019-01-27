9659016614b5e107367c5129ef1951aa66a3ac1e
hehomarie <marie.goudon@laposte.net>
2019-01-27 14:25:02 +0100

    modification dune ligne

--- index.rb
+++ index.rb
@@ -2,5 +2,6 @@ require 'rubygems'
 require 'nokogiri'
 require 'restclient'
 
-page = Nokogiri::HTML(RestClient.get("http://en.wikipedia.org/"))
+page = Nokogiri::HTML(RestClient.get("http://en.wikipedia.org/wiki/Cascading_Style_Sheets"))
 puts page.class 
+
